1123017cfb3cc9ab2d2e435d9bb9ccc375957e8a
Ashera2004 <79092592+Ashera2004@users.noreply.github.com>
2025-06-24 12:19:33 +0530

    Add files via upload

---
+++ echo_client.py
@@ -0,0 +1,9 @@
+#Name: A S Siddarth
+#Reg No. 212224040316
+import socket 
+s=socket.socket() 
+s.connect(('localhost',8000)) 
+while True: 
+    msg=input("Client > ") 
+    s.send(msg.encode()) 
+    print("Server > ",s.recv(1024).decode()) 
---
+++ echo_server.py
@@ -0,0 +1,10 @@
+#Name:A S Siddarth
+#Reg No. 212224040316
+import socket 
+s=socket.socket() 
+s.bind(('localhost',8000)) 
+s.listen(5) 
+c,addr=s.accept() 
+while True: 
+    ClientMessage=c.recv(1024).decode() 
+    c.send(ClientMessage.encode()) 
